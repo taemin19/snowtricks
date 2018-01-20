762061fd6ad8ae8546d05967e08208f6a08df7c7
taemin19 <daniel.thebault19@gmail.com>
2018-01-20 23:55:11 +0100

    Add custom method of the Trick repository

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,18 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Trick;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
+    const PER_PAGE = 6;
+
     /**
      * @Route("/", name="homepage")
      * @Method("GET")
      */
     public function indexAction()
     {
-        return $this->render('home/index.html.twig');
+        $repository =$this->getDoctrine()->getRepository(Trick::class);
+
+        $tricks = $repository->getLatest($page = 1,self::PER_PAGE, true);
+
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks
+        ]);
     }
 }

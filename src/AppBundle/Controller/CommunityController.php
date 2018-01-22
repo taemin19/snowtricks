4713596d2da8bad2e4ebc0329ea1123041f732a3
taemin19 <daniel.thebault19@gmail.com>
2018-01-22 14:46:03 +0100

    Add custom method to commnity Controller

--- src/AppBundle/Controller/CommunityController.php
+++ src/AppBundle/Controller/CommunityController.php
@@ -2,27 +2,62 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Trick;
+use AppBundle\Entity\Comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Controller used to manage community contents in the public part of the site.
+ *
+ * @Route("/snowboard")
+ */
 class CommunityController extends Controller
 {
+    const PER_PAGE = 6;
+
     /**
-     * @Route("/snowboard", name="trick_index")
+     * @Route("/", defaults={"page": "1"}, name="trick_index")
+     * @Route("/page/{page}", requirements={"page": "[1-9]\d*"}, name="trick_index_paginated")
      * @Method("GET")
      */
-    public function indexAction()
+    public function indexAction(int $page)
     {
-        return $this->render('community/index.html.twig');
+        $tricks = $this->getDoctrine()
+            ->getRepository(Trick::class)
+            ->getLatest($page,self::PER_PAGE, true)
+        ;
+
+        $nbPages = ceil(count($tricks) / self::PER_PAGE);
+
+        if ($page > $nbPages) {
+            throw $this->createNotFoundException('The page does not exist');
+        }
+
+        return $this->render('community/index.html.twig', [
+            'tricks' => $tricks,
+            'page' => $page,
+            'nbPages' => $nbPages
+        ]);
     }
 
     /**
-     * @Route("/snowboard/slug", name="trick_show")
+     * @Route("/tricks/{slug}", name="trick_show")
      * @Method("GET")
      */
-    public function showAction()
+    public function showAction(string $slug)
     {
-        return $this->render('community/show.html.twig');
+        $repository =$this->getDoctrine()->getRepository(Trick::class);
+
+        $trick = $repository->getOneBySlug($slug);
+
+        if (!$trick) {
+            throw $this->createNotFoundException('The trick does not exist');
+        }
+
+        return $this->render('community/show.html.twig', [
+            'trick' => $trick
+        ]);
     }
 }

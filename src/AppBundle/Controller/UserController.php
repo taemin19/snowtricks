5271a8b493f002f2375a693cbf1255ae7ccad367
taemin19 <daniel.thebault19@gmail.com>
2018-01-30 01:02:26 +0100

    Update UserController: save user, redirect, setFlash

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -16,9 +16,22 @@ class UserController extends Controller
      * @Route("/register", name="user_register")
      * @Method({"GET", "POST"})
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createForm(UserRegistrationForm::class);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $user = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Welcome '.$user->getUsername().'!');
+
+            return $this->redirectToRoute('homepage');
+        }
 
         return $this->render('user/register.html.twig', [
             'form' => $form->createView()

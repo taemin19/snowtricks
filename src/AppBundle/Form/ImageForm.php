58dc3adb8259c86b236b3686785e645dceaa0256
taemin19 <daniel.thebault19@gmail.com>
2018-02-05 18:58:01 +0100

    Create image form type

---
+++ src/AppBundle/Form/ImageForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file', FileType::class, [
+                'label_attr' => ['class' => 'sr-only'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class
+        ]);
+    }
+}

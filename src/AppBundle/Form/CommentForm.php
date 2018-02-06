0a614c21720fe07b0609ac64d3b008909e41d4df
taemin19 <daniel.thebault19@gmail.com>
2018-02-06 10:26:27 +0100

    Create comment form type

---
+++ src/AppBundle/Form/CommentForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('message', null);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class
+        ]);
+    }
+}

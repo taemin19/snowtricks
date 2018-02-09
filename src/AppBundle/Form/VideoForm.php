a9cfe0787be4089b5f56ddadc84d64f320cfb717
taemin19 <daniel.thebault19@gmail.com>
2018-02-09 14:22:09 +0100

    Update video form type

--- src/AppBundle/Form/VideoForm.php
+++ src/AppBundle/Form/VideoForm.php
@@ -12,7 +12,10 @@ class VideoForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('filePath', null);
+            ->add('filePath', null, [
+                'label' => 'Video url',
+                'attr' => ['placeholder' => 'e.g. https://www.youtube.com/embed/SQyTWk7OxSI']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

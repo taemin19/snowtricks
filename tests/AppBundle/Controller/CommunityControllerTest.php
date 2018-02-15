f82ffc56c354e5851094e67c08903bfc582196ff
taemin19 <daniel.thebault19@gmail.com>
2018-02-15 13:26:58 +0100

    Create functional test for the CommunityController

---
+++ tests/AppBundle/Controller/CommunityControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Functional test for the controllers defined inside CommunityController.
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class CommunityControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/snowboard/');
+
+        $this->assertCount(
+            6,
+            $crawler->filter('div.card'),
+            'The snowboard page displays the right number of tricks.'
+        );
+    }
+}
